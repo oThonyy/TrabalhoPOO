e96b811cac6a84d9ea7130afe405a99c95bab902
Anthony Oliveira <thony.sox@gmail.com>
2024-11-28 17:28:07 -0300

    DB Update

--- CrudPoo/database.sql
+++ CrudPoo/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE produtos (
     preco VARCHAR(10) NOT NULL,
     qntEstoque VARCHAR(5) NOT NULL,
     categoria VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE pedidos ( 
     pedidoId INT UNIQUE NOT NULL PRIMARY KEY,
@@ -18,11 +18,11 @@ CREATE TABLE pedidos (
     valorTotal VARCHAR(10) NOT NULL,
     formaPag VARCHAR(30) NOT NULL,
     statusPed VARCHAR(30) NOT NULL
-)
+);
 
 INSERT INTO produtos (produtoId, nome, descricao, preco, qntEstoque, categoria) VALUES
     ('1', 'SAMSUNG S20FE', 'Smartphone Samsung Galaxy S20FE 8/128gb processador octa core', '1249.99', '50', 'Eletrônicos'),
-    ('2', 'Poco X6 Pro', 'Smartphone PocoFone X6 Pro 12/512gb processador dimensity 8300', '1799.99', '15', 'Eletrônicos')
+    ('2', 'Poco X6 Pro', 'Smartphone PocoFone X6 Pro 12/512gb processador dimensity 8300', '1799.99', '15', 'Eletrônicos');
 
 INSERT INTO pedidos (pedidoId, produtos, dataPedido, valorTotal, formaPag, statusPed) VALUES 
     ('2', 'Poco X6 Pro', '23/11/2024', '1799.99', 'PIX', 'Em Processamento'),
